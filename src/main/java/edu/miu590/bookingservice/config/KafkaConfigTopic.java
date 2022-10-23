890a658f353a2b86c87bd421cd2b79a7ef4d33eb
devbinod <binodpant.nep@gmail.com>
2022-10-23 16:24:14 -0500

    zipkin added.

--- src/main/java/edu/miu590/bookingservice/config/KafkaConfigTopic.java
+++ src/main/java/edu/miu590/bookingservice/config/KafkaConfigTopic.java
@@ -3,8 +3,10 @@ package edu.miu590.bookingservice.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+@Configuration
 public class KafkaConfigTopic {
 
     @Value("${booking.notification-info.service.kafka.topic}")
